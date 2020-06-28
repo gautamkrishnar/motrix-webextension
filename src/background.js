b5d083dbb67122796db2355ff1f410dbb5eedc2a
Shreyansh Nayak <nayakshreyansh14@gmail.com>
2020-06-28 16:42:47 +0530

    feat: Replace Chrome APIs with WebExtensions APIs

--- src/background.js
+++ src/background.js
@@ -1,13 +1,13 @@
-'use strict';
+import Aria2 from './Aria2';
 
 function downloadAgent() {
-    chrome.downloads.onCreated.addListener(function (downloadItem) {
-        if (downloadItem.state !== "in_progress"){
+    browser.downloads.onCreated.addListener(function (downloadItem) {
+        if (downloadItem.state !== "in_progress") {
             return;
         }
-        // Triggered whenever a new download event fires
-        chrome.storage.sync.get(['motrixapikey', 'extensionstatus'], async function (result) {
-            if (!result.extensionstatus){
+
+        async function onGot(result) {
+            if (!result.extensionstatus) {
                 // Extension is disabled
                 return;
             }
@@ -27,10 +27,10 @@ function downloadAgent() {
                 const aria2 = new Aria2(options);
                 let downloadUrl = '';
                 // To support JS Downloads
-                if (validateUrl(downloadItem.url)){
-                    downloadUrl = downloadItem.url;
-                } else if(validateUrl(downloadItem.finalUrl)) {
+                if (validateUrl(downloadItem.finalUrl)) {
                     downloadUrl = downloadItem.finalUrl;
+                } else if (validateUrl(downloadItem.url)) {
+                    downloadUrl = downloadItem.url;
                 } else {
                     // Not a valid url: skip download
                     return;
@@ -56,14 +56,14 @@ function downloadAgent() {
                         out: filename
                     };
                 }
-                await aria2.call("addUri", [downloadUrl], params).then(async ()=> {
-                    // Added successfully: Cancels and removes the download from chrome download manager
-                    chrome.downloads.erase({id: downloadItem.id});
+                await aria2.call("addUri", [downloadUrl], params).then(async () => {
+                    // Added successfully: Cancels and removes the download from browser download manager
+                    browser.downloads.erase({ id: downloadItem.id });
                     // Shows notification
                     const notificationOptions = {
                         type: "basic",
                         iconUrl: "assets/images/icon-large.png",
-                        title: "Motrix Chrome Extension",
+                        title: "Motrix WebExtension",
                         message: "Download started on motrix download manger",
                         buttons: [
                             {
@@ -71,16 +71,25 @@ function downloadAgent() {
                             }
                         ]
                     };
-                    chrome.notifications.create( Math.round((new Date()).getTime() / 1000).toString(), notificationOptions);
-                    setTimeout(() => chrome.runtime.reload(), 1000);
+                    browser.notifications.create( Math.round((new Date()).getTime() / 1000).toString(), notificationOptions);
+                    setTimeout(() => browser.runtime.reload(), 1000);
                 }).catch((err) => {
                     console.log(err);
-                    // Failed: Show alert, Allows download to continue in chrome
+                    // Failed: Show alert, Allows download to continue in browser
                     alert("Motrix not installed or configured properly, Open Motrix set a API Key by visiting Preferences" +
                         " > Advanced > RPC Secret");
                 });
             }
-        });
+        }
+
+        async function onError(error) {
+            console.log(`Error: ${error}`);
+        }
+
+        // Triggered whenever a new download event fires
+        let getResult = browser.storage.sync.get(['motrixapikey', 'extensionstatus']);
+        getResult.then(onGot, onError);
+
     });
 }
 
@@ -88,11 +97,11 @@ function validateUrl(value) {
     return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
-chrome.runtime.onStartup.addListener(function () {
+browser.runtime.onStartup.addListener(function () {
     downloadAgent();
 });
 
 
-chrome.runtime.onInstalled.addListener(function () {
+browser.runtime.onInstalled.addListener(function () {
     downloadAgent();
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,30 +2,36 @@ const setButton = document.getElementById('setbtn');
 const keyInput = document.getElementById('motrixapikey');
 const extensionStatus = document.getElementById('extensionstatus');
 
-// Sets API key to null if it is not found else gets value of key and sets it to input box
-// Gets extension status and sets default value: true
-chrome.storage.sync.get(['motrixapikey', 'extensionstatus'], function (result) {
+function onGot(result) {
     console.log(result);
     if (!result.motrixapikey) {
-        chrome.storage.sync.set({motrixapikey: null});
+        browser.storage.sync.set({ motrixapikey: null });
         keyInput.value = '';
     } else {
         keyInput.value = result.motrixapikey;
     }
     if (result.extensionstatus === undefined) {
-        chrome.storage.sync.set({extensionstatus: true});
+        browser.storage.sync.set({ extensionstatus: true });
         extensionStatus.checked = true;
     } else {
         extensionStatus.checked = result.extensionstatus;
     }
-});
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+// Sets API key to null if it is not found else gets value of key and sets it to input box
+// Gets extension status and sets default value: true
+let getResult = browser.storage.sync.get(['motrixapikey', 'extensionstatus']);
+getResult.then(onGot, onError);
 
 // Saves the key to the storage
 setButton.addEventListener('click', () => {
-    chrome.storage.sync.set({motrixapikey: keyInput.value ? keyInput.value : null});
+    browser.storage.sync.set({ motrixapikey: keyInput.value ? keyInput.value : null });
     window.close();
 });
 
 extensionstatus.addEventListener('click', function (e) {
-    chrome.storage.sync.set({extensionstatus: e.target.checked});
+    browser.storage.sync.set({ extensionstatus: e.target.checked });
 });
