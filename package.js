5ea38a59d537b2f82396235340aa48bedfd07310
Shreyansh Nayak <nayakshreyansh14@gmail.com>
2020-07-01 15:36:16 +0530

    style: fix Styling issues based on Codacy

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ function zipChrome() {
     delete file['browser_specific_settings'];
     fs.writeFile(path.resolve(__dirname, 'dist', 'manifest.json'), JSON.stringify(file), function writeJSON(err) {
         if (err) {
-            return console.error(err)
+            return console.error(err);
         }
         zip('./dist', chromePath);
     });
