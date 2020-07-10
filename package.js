fba1e2d9e672b22b7bef69f91de4a814049d3c2f
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-07-11 00:17:23 +0530

    updated package script

--- package.js
+++ package.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const archiver = require('archiver');
 const filePath = path.resolve(__dirname, 'dist', 'manifest.json');
-const file = require(filePath);
 const packagePath = path.resolve(__dirname, 'packaged');
-var args = process.argv.slice(2);
-var settings = file['browser_specific_settings'];
+const file = require(filePath);
+let args = process.argv.slice(2);
+let settings = file['browser_specific_settings'];
 
 function archive(folder) {
-    var output = fs.createWriteStream(folder);
-    var archived = archiver('zip');
+    let output = fs.createWriteStream(folder);
+    let archived = archiver('zip');
     output.on('close', function () {
         console.log(archived.pointer() + ' total bytes');
         console.log('archiver has been finalized and the output file descriptor has closed.');
@@ -28,36 +28,25 @@ function archive(folder) {
         throw err;
     });
     archived.pipe(output);
-    archived.directory('dist/', false);
+    archived.directory('dist/', false, null);
     archived.finalize();
 }
 
-if (!fs.existsSync(packagePath)) {
-    console.log('Creating Folder ...');
-    fs.mkdirSync(packagePath);
-} else {
-    console.log('Deleting Folder ...');
-    fs.rmdirSync(packagePath, { recursive: true }, (err) => {
-        if (err) {
-            throw err;
-        }
-    });
-    console.log('Creating Folder ...');
-    fs.mkdirSync(packagePath);
-}
-
 args.forEach(function (val) {
-    var zip = val + '.zip';
-    var zipPath = path.resolve(packagePath, zip);
+    let zip = val + '.zip';
+    let zipPath = path.resolve(packagePath, zip);
+    if (fs.existsSync(zipPath)) {
+        fs.unlinkSync(zipPath);
+    }
     console.log('\n\'' + val + '\' target detected ...');
-    if (val == 'chrome') {
+    if (val === 'chrome') {
         console.log('Removing Browser Specific Settings from the manifest ...');
         delete file['browser_specific_settings'];
         fs.writeFileSync(filePath, JSON.stringify(file), function writeJSON(err) {
             if (err) {
                 return console.error(err);
             }
-        })
+        });
         console.log('Creating ' + zip + ' ...');
         archive(zipPath);
         console.log('Restoring Browser Specific Settings in the manifest ...');
